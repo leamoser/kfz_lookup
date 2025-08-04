d521354e2e5f3d3e0505c375cee03d2c2dd4845c
Lea Moser <privat@lea-moser.ch>
2025-08-04 17:30:12 +0200

    chore: update naming structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,18 @@
+/*DOM elements*/
 const MAIN = document.querySelector('main');
-const SHOW_LIST = document.querySelector('#show_list');
-const LIST_COUNT = document.querySelector('#count');
-const SEARCH = document.querySelector('#search');
-const CLEAR = document.querySelector('#clear');
-const CLOSE = document.querySelector('#close');
-const BADGE = document.querySelector('#badge');
-const TABLE = document.querySelector('#table');
-const LIST = document.querySelector('#list');
-const EXACT = document.querySelector('#exact');
-const MAP = document.querySelector('#map>svg');
-const LANDKREISE = MAP.querySelectorAll('.kreis');
+const ASIDE = document.querySelector('aside');
+const BTN_TOGGLE_LIST = document.querySelector('#btn_toggle_list');
+const BTN_CLEAR_SEARCH = document.querySelector('#btn_clear_search');
+const BTN_CLOSE_LIST = document.querySelector('#btn_close_list');
+const BTN_SEARCH_EXACT = document.querySelector('#btn_search_exact');
+const DATA_COUNT = document.querySelector('#data_count');
+const DATA_TABLE = document.querySelector('#data_table');
+const DATA_TOOLTIP = document.querySelector('#data_tooltip');
+const INPUT_SEARCH = document.querySelector('#input_search');
+const ITEM_SVG = document.querySelector('#item_svg');
+const ITEMS_LANDKREISE = ITEM_SVG.querySelectorAll('.kreis');
 
+/*init*/
 async function loadData() {
     try {
         const response = await fetch('./data/list.json');
@@ -24,21 +26,22 @@ const data = await loadData();
 let filtered = [];
 let ids = [];
 
+/*functions*/
 const renderList = () => {
-    TABLE.innerHTML = '';
+    DATA_TABLE.innerHTML = '';
     if (!filtered || filtered.length === 0) {
         const row = document.createElement('tr');
-        row.innerHTML = '<td class="empty">Keine Ergebnisse gefunden</td>';
-        TABLE.appendChild(row);
-        LIST_COUNT.innerText = '0';
+        row.innerHTML = '<td id="item_empty">Keine Ergebnisse gefunden</td>';
+        DATA_TABLE.appendChild(row);
+        DATA_COUNT.innerText = '0';
         return;
     };
     filtered.forEach((item) => {
         const row = document.createElement('tr');
         row.dataset.id = item.id;
         row.addEventListener('click', () => {
-            SEARCH.value = item.kennzeichen;
-            EXACT.dispatchEvent(new Event('click'));
+            INPUT_SEARCH.value = item.kennzeichen;
+            BTN_SEARCH_EXACT.dispatchEvent(new Event('click'));
         })
         const kennzeichen = document.createElement('td');
         kennzeichen.innerText = item.kennzeichen;
@@ -52,12 +55,12 @@ const renderList = () => {
         const ursprung = document.createElement('td');
         ursprung.innerText = item.ursprung;
         row.appendChild(ursprung);
-        TABLE.appendChild(row);
+        DATA_TABLE.appendChild(row);
     })
-    LIST_COUNT.innerText = filtered.length;
+    DATA_COUNT.innerText = filtered.length;
 }
 const renderMap = () => {
-    LANDKREISE.forEach((landkreis) => {
+    ITEMS_LANDKREISE.forEach((landkreis) => {
         const id = parseInt(landkreis.dataset.name);
         if (ids.includes(id)) {
             landkreis.classList.add('active');
@@ -70,7 +73,7 @@ const renderMap = () => {
 const clearSearch = () => {
     filtered = [];
     ids = [];
-    LIST.classList.remove('visible');
+    ASIDE.classList.remove('visible');
     MAIN.classList.remove('listed');
     renderList();
     renderMap();
@@ -81,7 +84,7 @@ const findAll = (query) => {
     })
     const mapped = filtered.map((item) => item.landkreis_id)
     ids = [...new Set(mapped)];
-    LIST.classList.add('visible');
+    ASIDE.classList.add('visible');
     MAIN.classList.add('listed');
     renderList();
     renderMap();
@@ -92,75 +95,79 @@ const findExact = (query) => {
     })
     const mapped = filtered.map((item) => item.landkreis_id)
     ids = [...new Set(mapped)];
-    LIST.classList.add('visible');
+    ASIDE.classList.add('visible');
     MAIN.classList.add('listed');
     renderList();
     renderMap();
 }
 
-SEARCH.addEventListener('input', () => {
+/*events*/
+INPUT_SEARCH.addEventListener('input', () => {
     if (!data) return;
-    const query = SEARCH.value.toUpperCase();
-    SEARCH.value = SEARCH.value.toUpperCase();
+    const query = INPUT_SEARCH.value.toUpperCase();
+    INPUT_SEARCH.value = INPUT_SEARCH.value.toUpperCase();
     if (query) {
         findAll(query);
     } else {
         clearSearch();
     }
 });
-EXACT.addEventListener('click', () => {
+BTN_SEARCH_EXACT.addEventListener('click', () => {
     if (!data) return;
-    const query = SEARCH.value.toUpperCase();
-    SEARCH.value = SEARCH.value.toUpperCase();
+    const query = INPUT_SEARCH.value.toUpperCase();
+    INPUT_SEARCH.value = INPUT_SEARCH.value.toUpperCase();
     if (query) {
         findExact(query);
     } else {
         clearSearch()
     }
 })
-CLEAR.addEventListener('click', () => {
-    SEARCH.value = '';
+BTN_CLEAR_SEARCH.addEventListener('click', () => {
+    INPUT_SEARCH.value = '';
     clearSearch();
 })
-CLOSE.addEventListener('click', () => {
-    SEARCH.value = '';
-    clearSearch();
+BTN_CLOSE_LIST.addEventListener('click', () => {
+    ASIDE.classList.remove('visible');
+    MAIN.classList.remove('listed');
 })
-SHOW_LIST.addEventListener('click', () => {
-    // if (!filtered.length) return;
-    LIST.classList.add('visible');
-    MAIN.classList.add('listed');
+BTN_TOGGLE_LIST.addEventListener('click', () => {
+    if (ASIDE.classList.contains('visible')) {
+        ASIDE.classList.remove('visible');
+        MAIN.classList.remove('listed');
+    } else {
+        ASIDE.classList.add('visible');
+        MAIN.classList.add('listed');
+    }
 })
 window.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') {
-        SEARCH.value = '';
+        INPUT_SEARCH.value = '';
         document.activeElement.blur();
         clearSearch();
     }
     if (e.key === 'k' && e.metaKey) {
-        SEARCH.focus();
+        INPUT_SEARCH.focus();
     }
 })
-
-LANDKREISE.forEach((LANDKREIS) => {
+ITEMS_LANDKREISE.forEach((LANDKREIS) => {
     LANDKREIS.addEventListener('click', () => {
         const id = parseInt(LANDKREIS.dataset.name);
         const landkreis = data.find(item => item.id === id);
         if (!landkreis) return;
-        BADGE.classList.add('inactive')
-        SEARCH.value = landkreis.kennzeichen;
-        EXACT.dispatchEvent(new Event('click'));
+        DATA_TOOLTIP.classList.add('inactive')
+        INPUT_SEARCH.value = landkreis.kennzeichen;
+        BTN_SEARCH_EXACT.dispatchEvent(new Event('click'));
     })
     LANDKREIS.addEventListener('mouseover', (e) => {
         const id = parseInt(LANDKREIS.dataset.name);
         const landkreise = data.filter(item => item.landkreis_id === id);
         if (!landkreise.length) return;
-        BADGE.innerHTML = landkreise.map(landkreis => landkreis.kennzeichen).join(', ');
-        BADGE.style.left = `${e.clientX + 10}px`;
-        BADGE.style.top = `${e.clientY - 10}px`;
-        BADGE.classList.remove('inactive')
+        DATA_TOOLTIP.innerHTML = landkreise.map(landkreis => landkreis.kennzeichen).join(', ');
+        DATA_TOOLTIP.style.left = `${e.clientX + 10}px`;
+        DATA_TOOLTIP.style.top = `${e.clientY - 10}px`;
+        DATA_TOOLTIP.classList.remove('inactive')
     })
     LANDKREIS.addEventListener('mouseleave', () => {
-        BADGE.classList.add('inactive')
+        DATA_TOOLTIP.classList.add('inactive')
     })
 })
